1dc835a211a71755ea527d587aadf43a0fc5c17a
Ahmed Salah <ahmedsalah9570@gmail.com>
2023-02-05 20:54:28 +0200

    fix view

--- resources/views/productservice/index.blade.php
+++ resources/views/productservice/index.blade.php
@@ -89,11 +89,12 @@
                                             @php
                                                 $taxes=\App\Models\Utility::tax($productService->tax_id);
                                             @endphp
-
+                                            @if(!empty($taxes))
                                             @foreach($taxes as $tax)
                                                 <span class="">{{$tax->name .' ('.$tax->rate .'%)'}}</span><br>
 
                                             @endforeach
+                                            @endif
                                         @else
                                             -
                                         @endif
