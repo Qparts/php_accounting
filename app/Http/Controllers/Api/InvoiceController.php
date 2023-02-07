e438a1cd9ae99701c4f4e57965f33aa13a063ac1
Ahmed Salah <ahmedsalah9570@gmail.com>
2023-02-07 11:53:52 +0200

    fix

--- app/Http/Controllers/Api/InvoiceController.php
+++ app/Http/Controllers/Api/InvoiceController.php
@@ -69,7 +69,8 @@ class InvoiceController extends Controller
                 $invoiceProduct->invoice_id  = $invoice->id;
                 $invoiceProduct->product_id  = $productBySKU->id;
                 $invoiceProduct->quantity    = $products[$i]['quantity'];
-                $invoiceProduct->tax         = $products[$i]['tax_percent'];
+           //     $invoiceProduct->tax         = $products[$i]['tax_percent'];
+                $invoiceProduct->tax         = 0;
                 $invoiceProduct->discount    = $products[$i]['discount'];
                 $invoiceProduct->price       = $products[$i]['unit_price'];
                 $invoiceProduct->description = $products[$i]['description'];
